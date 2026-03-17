67b1ae92762c126eb9ea7b5974bb32d115a85037
Meikelele <michal.bak2002@interia.pl>
2026-03-17 21:27:57 +0100

    feat(Global Statistic): added global statistic DTO

---
+++ src/main/java/pl/mbak/filesmanager/dto/GlobalStatisticsResponse.java
@@ -0,0 +1,32 @@
+package pl.mbak.filesmanager.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Schema(description = "Response for global statistics")
+public class GlobalStatisticsResponse {
+
+    @Schema(description = "Total count of folders")
+    private Long totalFolders;
+
+    @Schema(description = "Total count of files")
+    private Long totalFiles;
+
+    @Schema(description = "Size of all files")
+    private Long totalSize;
+
+    @Schema(description = "Average file size")
+    private Double avgFileSize;
+
+    @Schema(description = "The longest file name")
+    private String largestFileName;
+
+    @Schema(description = "The biggest file size")
+    private Long largestFileSize;
+
+}
